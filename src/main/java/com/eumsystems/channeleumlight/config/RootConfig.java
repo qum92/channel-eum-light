18242bcf885f173312082802d39cf370e2883fdd
qum92 <oscar9992@naver.com>
2021-04-19 17:50:45 +0900

    initial import

---
+++ src/main/java/com/eumsystems/channeleumlight/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.eumsystems.channeleumlight.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"com.eumsystems.channeleumlight"})
+public class RootConfig {
+
+}
